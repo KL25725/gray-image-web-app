e12ac37765ed162709d49644f1a65688acfcdeda
KL <klpy25725@gmail.com>
2025-08-12 23:40:07 +0800

    Include brightness slider

--- grayimage_web.py
+++ grayimage_web.py
@@ -1,10 +1,18 @@
 import streamlit as st
-from PIL import Image
+from PIL import Image, ImageEnhance
+
+def adjust_brightness(image, factor):
+    enhancer = ImageEnhance.Brightness(image)
+    return enhancer.enhance(factor)
+
+
 
 
 st.subheader("Color to Grayscale Converter")
 
 uploaded_image = st.file_uploader("Upload Image")
+
+
 
 with st.expander("Open lens"):
     camera_image = st.camera_input('Camera')
@@ -15,9 +23,18 @@ if camera_image:
 
     gray_img = img.convert("L")
 
-    st.image(gray_img)
+    #st.image(gray_img)
+
+    brightness_factor = st.slider("Brightness", 0.0, 2.0, 1.0, 0.1,key="slider1")
+
+    adjusted_image1 = adjust_brightness(gray_img, brightness_factor)
+    st.image(adjusted_image1, caption=f"Adjusted Image (Brightness: {brightness_factor})", use_container_width=True)
 
 if uploaded_image:
     userimg =Image.open(uploaded_image)
     gray_userimg = userimg.convert("L")
-    st.image(gray_userimg)
+    #st.image(gray_userimg)
+    brightness_factor = st.slider("Brightness", 0.0, 2.0, 1.0, 0.1, key="slider2")
+
+    adjusted_image2 = adjust_brightness(gray_userimg, brightness_factor)
+    st.image(adjusted_image2, caption=f"Adjusted Image (Brightness: {brightness_factor})", use_container_width=True)
